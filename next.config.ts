c3bbfa8d652b79be6e84d022f6ea5b439e179db8
Mahmoud Alfaramawy <101274098+Ma7moud-Emad@users.noreply.github.com>
2025-12-22 10:58:11 +0200

    Enable image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: [
-  //     "images.unsplash.com",
-  //     "vvmwizqlurbngruwobya.supabase.co",
-  //     "www.youtube.com",
-  //   ],
-  // },
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "vvmwizqlurbngruwobya.supabase.co",
+      "www.youtube.com",
+    ],
+  },
 };
 
 export default nextConfig;
